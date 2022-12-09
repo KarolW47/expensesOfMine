6e3016b64fe8ab2f70436b13bbb59e6c51b420fc
KaWa <Karollo47@gmail.com>
2022-12-09 19:50:06 +0100

    first commit

---
+++ src/test/java/pl/kawatojava/expences/ExpencesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kawatojava.expences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
